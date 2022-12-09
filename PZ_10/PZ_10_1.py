98b5633016b5b79f9c9de3fd3a84ce2b29b7668e
ergudz <114377170+ergudz@users.noreply.github.com>
2022-12-09 20:37:24 +0300

    Add files via upload

--- PZ_10/PZ_10_1.py
+++ PZ_10/PZ_10_1.py
@@ -4,23 +4,20 @@
 # 2. в каких магазинах можно приобрести одновременно молоко и сахар.
 # 3. в каких магазинах можно приобрести соль.
 
-shops = {
-    'Магнит': ['молоко', 'соль', 'сахар', ],
-    'Пятерочка': ['мясо', 'молоко', 'сыр', ],
-    'Перекресток': ['молоко', 'творог', 'сыр', 'сахар', ],
-}
-shops_havent_cheese = []
-shops_with_milk_cheese = []
-shops_with_solt = []
+d = {'Магнит': {'молоко', 'соль', 'сахар'},
+     'Пятерочка': {'мясо', 'молоко', 'сыр'},
+     'Перекресток': {'молоко', 'творог', 'сыр', 'сахар'}}  # Объявление словаря
 
-for x in shops.keys():
-    if 'сыр' not in shops[x]:
-        shops_havent_cheese.append(x)
-    if 'молоко' in shops[x] and 'сахар' in shops[x]:
-        shops_with_milk_cheese.append(x)
-    if 'соль' in shops[x]:
-        shops_with_solt.append(x)
+set1, set2, set3 = set(), set(), set()  # Создание пустых множеств
 
-print(f'Магазины в которых нельзя приобрести сыр: {(", ".join(shops_havent_cheese))}')
-print(f'Магазины в которых можно приобрести одновременно молоко и сахар: {(", ".join(shops_with_milk_cheese))}')
-print(f'Магазины в которых можно приобрести соль: {(", ".join(shops_with_solt))}')
+for key in d:  # Цикл, перебирающий ключи словаря
+    if 'сыр' not in d[key]:
+        set1 |= {key}  # Добавление ключа в множество
+    if 'молоко' in d[key] and 'сахар' in d[key]:
+        set2 |= {key}
+    if 'соль' in d[key]:
+        set3 |= {key}
+
+print('1.', ', '.join(set1))  # Вывод результата
+print('2.', ', '.join(set2))
+print('3.', ', '.join(set3))
