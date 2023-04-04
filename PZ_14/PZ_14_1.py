68b73bf9130a640376fcd1eb30cd41acc913bd91
ergudz <114377170+ergudz@users.noreply.github.com>
2023-04-05 02:06:40 +0300

    Add files via upload

--- PZ_14/PZ_14_1.py
+++ PZ_14/PZ_14_1.py
@@ -1,7 +1,5 @@
-# В исходном текстовом файле(Dostoevsky.txt) найти все произведения писателя.
-# Посчитать количество полученных элементов.
 import re  # импорт модуля с регулярными выражениями
 
-with open("Dostoevsky.txt") as file:  # Открываю текствовый файл
+with open("Dostoevsky.txt", encoding="utf-8") as file:  # Открываю текствовый файл
     data = len(set(re.findall(r'(?<=«)[\w\s]+', file.read())))  # Считаю кол-во произведений писателя
 print(data)  # Вывод результата
